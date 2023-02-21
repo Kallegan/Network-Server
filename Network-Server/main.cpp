b90d3be9a780960e85f9231d1a748ee069863884
Kallegan <90606627+Kallegan@users.noreply.github.com>
2023-02-21 01:03:05 +0100

    loop to keep server running, multi threading to enable multiple clients

--- Network-Server/main.cpp
+++ Network-Server/main.cpp
@@ -1,16 +1,40 @@
 #include <stdio.h>
 #include <WinSock2.h> //used for open sockets on operative system.
+#include <Windows.h>
 
+
+
+DWORD client_loop(void* ptr)
+{
+	SOCKET client = reinterpret_cast<SOCKET>(ptr);
+	while (true)
+	{
+		char buffer[1024];
+		int recv_size = recv(client, buffer, 1024, 0);
+
+		if (recv_size == -1)
+		{
+			printf("Client Disconnected!");
+			return 0;
+		}
+
+		printf("%.*s\n", recv_size, buffer);
+	}
+	return 0;
+}
 
 int main()
 {
+	
+	
+
 	//version 2.2,
 	WSADATA wsa_data; //data struct
 	WSAStartup(MAKEWORD(2, 2), &wsa_data); //initialize winsock lib.
 
-	printf("Enter port: ");
-	unsigned short port;
-	scanf_s("%hu", &port); //%hu = reads half unsigned (unsigned short).
+	//printf("Enter port: ");
+	unsigned short port = 49;
+	//scanf_s("%hu", &port); //%hu = reads half unsigned (unsigned short).
 
 	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
@@ -34,17 +58,19 @@ int main()
 	//sets listen sock to listen mode, and to allow int amount of connections.
 	listen(listen_sock, 5);
 
-	//blocking function. Blocks until a connection is established. Then returns socket.
-	SOCKET client = accept(listen_sock, NULL, NULL);
+	
+	
 
-	char read_buffer[1024];
-	int read_len = recv(client, read_buffer, 1024, 0);
-	read_buffer[read_len] = 0;
-
-	printf("Client MSG: %s", read_buffer);
-
-	const char* response = "CONNECTED TO SERVER!";
-	send(client, response, strlen(response), 0);
-
-	return 0;
+	while (true)
+	{
+		SOCKET client = accept(listen_sock, NULL, NULL); //blocking function. Blocks until a connection is established.
+		CreateThread(
+			nullptr,
+			0,
+			client_loop,
+			(void*)client, //casts the value to a void ptr to avoid client value going out of scope before thread is created.
+			0,
+			nullptr
+		);
+	}
 }
